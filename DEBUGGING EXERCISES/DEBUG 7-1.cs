32a8f480a5ccc2d0c38b3a2582cb5425eb3fda6c
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-11-07 11:20:38 -0600

    Create DEBUG 7-1.cs

---
+++ DEBUGGING EXERCISES/DEBUG 7-1.cs
@@ -0,0 +1,19 @@
+using static System.Console;
+class DebugSeven1
+{
+   static void Main()
+   {
+      DisplayWebAddress();
+      WriteLine("Shop at Shopper's World");
+      DisplayWebAddress();
+      WriteLine("The best bargains from around the world");
+      DisplayWebAddress();
+   }
+   public static void DisplayWebAddress()
+   {
+      WriteLine("------------------------------");
+      WriteLine("Visit us on the web at:");
+      WriteLine("www.shoppersworldbargains.com");
+      WriteLine("******************************");
+   }
+}
